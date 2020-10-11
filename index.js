140277ea44dc779dbe43335962ebc63331af9892
Daniel Stahl <danstahl1138@gmail.com>
2020-10-11 08:24:13 -0500

    removed need for results.json

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-const {spawn} = require('child_process')
-const results=require('./results.json')
-results.options_and_rate.forEach(v=>{
-    v.options=v.options.filter(({price})=>price>0)
-    v.options.sort((a, b)=>a.strike-b.strike)
-})
+const { spawn } = require('child_process')
 
-console.log(results.options_and_rate[2].options)
-const generateModel=index=>new Promise((resolve, reject)=>{
-    const model=spawn('./target/release/fang_oost_cal_charts', [index, JSON.stringify({...results, constraints:{}})])
-    let modelOutput=''
-    let modelErr=''
-    model.stdout.on('data', data=>{
-        modelOutput+=data
+const generateModel = index => new Promise((resolve, reject) => {
+    const model = spawn('./target/release/fang_oost_cal_charts', [index, JSON.stringify({ constraints: {} })])
+    let modelOutput = ''
+    let modelErr = ''
+    model.stdout.on('data', data => {
+        modelOutput += data
     })
-    model.stderr.on('data', data=>{
-        modelErr+=data
+    model.stderr.on('data', data => {
+        modelErr += data
     })
-    model.on('close', code=>{
-        
-        if(modelErr){
+    model.on('close', code => {
+
+        if (modelErr) {
             reject(modelErr)
         }
         else {
@@ -27,8 +21,8 @@ const generateModel=index=>new Promise((resolve, reject)=>{
     })
 })
 
-Promise.all([0, 1, 2, 3].map(generateModel))
-    .then(res=>console.log(res))
-    .catch(err=>{
+Promise.all([0, 1, 2].map(generateModel))
+    .then(res => console.log(res))
+    .catch(err => {
         console.log(err)
     })
